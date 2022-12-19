d0631fd6c398768c03e7781386b408921f06dc42
Valere Youbi <v.youbi@aftech-solution.com>
2022-12-19 11:45:34 +0100

    Cross origin edited

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  server: 'http://ae580cfe0d60c4b96acfd2d861a5ff14-1474643582.eu-central-1.elb.amazonaws.com/api'
+  server: 'http://a1b85fb2040294f0fbdc59a359db27b9-1298126156.eu-west-2.elb.amazonaws.com/api'
 };
